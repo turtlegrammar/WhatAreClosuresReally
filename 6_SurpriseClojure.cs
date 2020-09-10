8c65a961f3c2472e22994e0f7bd4863072d33dbd
Joseph DiSalvo <jdisalvo@projetech.com>
2020-09-09 22:39:49 -0400

    improved parser

--- 6_SurpriseClojure.cs
+++ 6_SurpriseClojure.cs
@@ -26,69 +26,54 @@ namespace Closures
 
         public static Parser<List<string>> ParseArgsList =
             from open in ch('(')
-            from spaces1 in spaces 
             from ids in  many(between(spaces, spaces, Identifier))
             from close in ch(')')
             select ids.ToList();
 
         public static Parser<Exp> ParseCreateFunction =
             from fnKeyword in str("function")
-            from spaces1 in spaces 
-            from argList in ParseArgsList
-            from spaces2 in spaces
+            from argList in between(spaces, spaces, ParseArgsList)
             from body in many1(ParseExp)
             select Function(argList, AsSeq(body.ToArray()));
 
         public static Parser<Exp> ParseAssign =
             from assignKeyword in str("assign")
-            from spaces1 in spaces
-            from lvalue in Identifier
-            from spaces2 in spaces
+            from lvalue in between(spaces, spaces, Identifier)
             from rvalue in ParseExp 
             select Assign(lvalue, rvalue);
 
         public static Parser<Exp> ParseIf = 
             from ifKeyword in str("if")
-            from spaces1 in spaces 
-            from condition in ParseExp
-            from spaces2 in spaces 
-            from trueBranch in ParseExp 
-            from spaces3 in spaces 
+            from condition in between(spaces, spaces, ParseExp)
+            from trueBranch in between(spaces, spaces, ParseExp)
             from falseBranch in ParseExp
             select If(condition, trueBranch, falseBranch);
 
         public static Parser<Exp> ParseWhile = 
             from whileKeyword in str("while")
-            from spaces1 in spaces 
-            from condition in ParseExp
-            from spaces2 in spaces 
+            from condition in between(spaces, spaces, ParseExp)
             from body in many1(ParseExp)
-            from spaces3 in spaces 
             select While(condition, AsSeq(body.ToArray()));
 
         public static Parser<Exp> ParseSpecialFormOrFnApplication = 
             from open in ch('(')
-            from spaces2 in spaces 
-            from result in choice(
+            from result in between(spaces, spaces, choice(
                 attempt(ParseCreateFunction),
                 attempt(ParseAssign),
                 attempt(ParseIf),
                 attempt(ParseWhile),
                 from list in many1(ParseExp) select FunctionCall(list.First(), list.Skip(1).ToArray())
-            )
-            from spaces3 in spaces
+            ))
             from close in ch(')')
             select result;
 
 
         public static Parser<Exp> ParseExp = 
-            from s1 in spaces
-            from result in choice(
+            from result in between(spaces, spaces, choice(
                 attempt(ParseNumber),
                 attempt(ParseSymbol),
                 ParseSpecialFormOrFnApplication
-            )
-            from s2 in spaces 
+            ))
             select result;
 
         public static Exp Parse(string code)
